37907883dcc5d2c0a774b2e0583ed7c07ef4a782
GauriVispute <gaurivispute92@gmail.com>
2026-02-15 22:24:08 +0530

    java file

---
+++ Variable_and_Datatype/Sum_of_two_numbers.java
@@ -0,0 +1,10 @@
+package Variable_and_Datatype;
+
+public class Sum_of_two_numbers {
+    public static void main(String abc[]){
+        int a =10;
+        int b=5;
+        int sum = a+b;
+        System.out.print("Sum : "+sum);
+    }
+}
---
+++ Variable_and_Datatype/VariableInJava.java
@@ -0,0 +1,21 @@
+package Variable_and_Datatype;
+
+public class VariableInJava {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 5 ;
+        System.out.println("a");
+
+        System.out.println(a);
+        System.out.println(b);
+
+        String name  = "Tony Shark";
+        System.out.println(name);
+
+         a = 50 ;
+          System.out.println(a);
+
+          a = b;
+           System.out.println(a);
+    }
+}
