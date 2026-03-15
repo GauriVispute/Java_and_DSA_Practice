28cc30a846f9bed744e54f7d8b33c18209a4b5f1
GauriVispute <gaurivispute92@gmail.com>
2026-03-15 13:47:41 +0530

    java file

---
+++ Variable_and_Datatype/Type_casting.java
@@ -0,0 +1,64 @@
+package Variable_and_Datatype;
+
+import java.util.Scanner;
+public class Type_casting{
+    public static void main(String [] args){
+       /* 
+       float a = 25.0;
+       int b = a ; // not allowed
+       System.out.println(b);
+       
+       // type casting 
+       float a = 25.0f;
+       int b = (int) a; // 
+       System.out.println(b);
+
+
+       // non-crucial data // non-critical data 
+       float a = 25.999999f;
+       int b  = (int ) a;
+       System.out.println(b);
+       
+       // crucial data// critical data
+       // loss of critical data 
+       Scanner sc = new Scanner (System.in);
+       float marks = 99.99999f;
+       int marks2  =   (int)marks ;
+       
+       System.out.println(marks2);
+       
+       // non-crucial // non-critial data 
+       float number = 99.9999f;
+       int number2  = (int) number;
+       
+       System.out.println(number2);
+       */
+
+      //  for characters to intergers 
+        
+      
+    /*
+        Charaters has Ascii values
+        char->number(possible)
+    */
+        /* 
+        // allowed for characters
+        char ch ='0';
+        int number = ch;
+        System.out.println(number );
+        
+        
+        char ch1 = 'a';
+        char ch2 = 'b';
+        int number1 = ch1;
+        int number2 = ch2;
+        System.out.println(number1);
+        System.out.println(number2);
+        
+        */
+
+        char ch= 'A';
+        int number  = ch;
+        System.out.println(number);
+    }
+}
