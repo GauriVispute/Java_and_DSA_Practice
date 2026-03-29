af83c48b2d89187c97e28911adad56ab0302cdad
GauriVispute <gaurivispute92@gmail.com>
2026-03-29 14:21:34 +0530

    new folder

---
+++ Operators/Unary_operator.java
@@ -0,0 +1,48 @@
+package Operators;
+
+public class Unary_operator{
+    public static void main(String abc []){
+        /* Pre-Increment  */
+        // int a = 10;
+        // int b = ++a;
+        
+        /*
+            1. change the value
+            2. Use the value
+        */
+
+        /* Post-Increment */
+
+        // int a = 10;
+        // int b = a++;
+
+        /* 
+            1. use the value
+            2.change the value
+        */
+
+        /* Pre-Decrement */
+
+        // int a = 10; // 9
+        // int b = --a; // 9
+
+        /*
+            1. Change the value
+            2. use the value
+        */
+
+
+        /* Post-Decrement */
+
+        int a = 10; // 9
+        int b = a--; // 10
+        
+        /*
+            1.use the value
+            2. change the value
+        */
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
