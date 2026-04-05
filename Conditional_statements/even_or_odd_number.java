dc08b0714b1750f98b0075d3ab06b7706058d2a7
GauriVispute <gaurivispute92@gmail.com>
2026-04-05 21:03:34 +0530

    java file

--- Conditional_statements/even_or_odd_number.java
+++ Conditional_statements/even_or_odd_number.java
@@ -4,11 +4,11 @@ public class even_or_odd_number{
     public static void main(String [] abc){
         int number = 79;
 
-        // if(number % 2 == 0){
-        //     System.out.println("Even Number");
-        // } else {
-        //     System.out.println("Odd Number");
-        // }
+        if(number % 2 == 0){
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
 
         // using bitwise and operator
 
