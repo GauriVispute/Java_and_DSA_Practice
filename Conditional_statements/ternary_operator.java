8b86c6417c9ad805bf1ac16e388d16b71d0fae50
GauriVispute <gaurivispute92@gmail.com>
2026-04-14 22:14:06 +0530

    java file

--- Conditional_statements/ternary_operator.java
+++ Conditional_statements/ternary_operator.java
@@ -2,8 +2,15 @@ package Conditional_statements;
 
 public class ternary_operator{
     public static void main(String [] abc){
-        int value = (5>3)? 5 : 3;
 
-        System.out.println("Value : "+value);
+        int number =4;
+
+        String type  = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("Number is "+type);
+        
+        
+        // int value = (5>3)? 5 : 3;
+        // System.out.println("Value : "+value);
+
     }
 }
