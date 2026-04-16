6e8ce517de9cf63889ef76e27be675fe31a683c6
GauriVispute <gaurivispute92@gmail.com>
2026-04-16 13:59:22 +0530

    ternary operator

--- Conditional_statements/check_student_is_pass_or_fail_by_ternary_operator.java
+++ Conditional_statements/check_student_is_pass_or_fail_by_ternary_operator.java
@@ -2,7 +2,7 @@ package Conditional_statements;
 
 public class check_student_is_pass_or_fail_by_ternary_operator {
     public static void main(String[] args) {
-        int marks = 33;
+        int marks = 97;
 
         String result = (marks>=33)?"Pass":"Fail";
         System.out.println("Result : "+result);
