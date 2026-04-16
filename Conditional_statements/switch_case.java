3c1946eb3aad1af273ec1c28caddd4ea896854b4
GauriVispute <gaurivispute92@gmail.com>
2026-04-16 14:48:03 +0530

    switch case question

---
+++ Conditional_statements/switch_case.java
@@ -0,0 +1,19 @@
+package Conditional_statements;
+
+public class switch_case {
+    public static void main(String [] args){
+        int number  = 7;
+
+        switch(number){
+            case 1 : System.out.println("Samosa");
+            break;
+            case 2 : System.out.println("Kachori");
+            break;
+            case 3 : System.out.println("Mandu vada");
+            break;
+            case 4 : System.out.println("Chocolate Milkshake with Ice-Cream");
+            break;
+            default  : System.out.println("HomeMade Food") ;
+        }
+    }
+}
