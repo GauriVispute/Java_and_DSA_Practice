1908ae9c08246fd2880ed36b47f34ea211a53ce4
GauriVispute <gaurivispute92@gmail.com>
2026-04-19 21:39:54 +0530

    java file

---
+++ Loops/while_loop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int counter =0 ;
+        while(counter<100){
+            System.out.println(counter+" Hello World");
+            counter++;
+        }
+    }
+}
