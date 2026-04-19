8f8c50f77e9d026bd901eb85c81fb5e2755ed5da
GauriVispute <gaurivispute92@gmail.com>
2026-04-19 21:39:11 +0530

    java file

--- Conditional_statements_extra_question/check_leap_year.java
+++ Conditional_statements_extra_question/check_leap_year.java
@@ -2,17 +2,21 @@ package Conditional_statements_extra_question;
 
 public class check_leap_year {
     public static void main(String [] abc){
-        int year =2004;
-
-        if ((year % 4 ) == 0  || (year % 100 ) == 0 || (year % 400 ) == 0){
-                        System.out.println(year+" It a Leap year !!");
-            } else {
-                System.out.println(year+" Not a Leap Year !!");
-            } 
+        int year =2000;
+        
+        boolean x = (year % 4 ) == 0 ;
+        boolean y = (year % 100 ) != 0;
+        boolean z = (year % 100 ) == 0 && (year % 400 ) == 0;
+        
+        if ( x && (y || z) ){
+            System.out.println(year+" It a Leap year !!");
+        } else {
+            System.out.println(year+" Not a Leap Year !!");
+        } 
 
             // Display all the leap year in range 
         /* for(year = 1950 ;year<=2030;year++){
-            if ((year % 4 ) == 0  || (year % 100 ) == 0 || (year % 400 ) == 0){
+            if (x&(y||z)){
                         System.out.println(year+" It a Leap year !!");
             } else {
                 //System.out.println("Not a Leap Year !!");
