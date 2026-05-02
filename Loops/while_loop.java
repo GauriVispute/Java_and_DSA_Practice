712a16bf84a2fd79719df2ee80d0adee6a5682d4
GauriVispute <gaurivispute92@gmail.com>
2026-05-02 06:07:00 +0530

    java file

--- Loops/while_loop.java
+++ Loops/while_loop.java
@@ -3,9 +3,11 @@ package Loops;
 public class while_loop {
     public static void main(String[] args) {
         int counter =0 ;
-        while(counter<100){
+        while(counter<100000){
             System.out.println(counter+" Hello World");
             counter++;
         }
+
+        System.out.println("------- Printed HW "+counter+"x -------");
     }
 }
