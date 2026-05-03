968e11f4b061c2b7f63820b22226154b0834f8d0
GauriVispute <gaurivispute92@gmail.com>
2026-05-03 19:11:54 +0530

    while loop Qn

---
+++ Loops/print_1_to_n_no_using_while_loop.java
@@ -0,0 +1,19 @@
+package Loops;
+
+import java.util.Scanner;
+public class print_1_to_n_no_using_while_loop {
+    public static void main(String [] args){
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+
+        int counter =1;
+        while(counter<=n){
+            System.out.println(counter);
+            counter++;
+        }
+
+        sc.close();
+    }
+}
