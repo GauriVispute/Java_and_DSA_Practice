a27286996a71fe75093b3dd27abdbdc2463c62be
GauriVispute <gaurivispute92@gmail.com>
2026-05-03 18:56:14 +0530

    While Loop Qn

---
+++ Loops/print_1_to_10_no_using_while_loop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class print_1_to_10_no_using_while_loop{
+    public static void main(String [] args){
+        int counter= 1;
+        while(counter <= 10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
