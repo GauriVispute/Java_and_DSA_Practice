dd886cfd8251510dfd037f816e441a49772a715e
Gauri Vijay Vispute <104923499+GauriVispute@users.noreply.github.com>
2026-05-08 09:19:03 +0530

    Create sum_of_all_natural_numbers.java
    sum_of_all_natural_numbersusing_while_loop

---
+++ Loops/sum_of _all_natural_numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class sum_of_all_natural_numbers{
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number : ");
+		int n = sc.nextInt();
+		
+		int counter = 1;
+		int sum =0;
+		while(counter<=n){
+			sum= sum+counter;
+			counter++;
+		}
+		
+		System.out.println("Sum : "+sum);;
+  }
+}
