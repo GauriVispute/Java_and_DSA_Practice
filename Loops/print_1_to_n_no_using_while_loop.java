23d86a0c66a965b7e0dd9ff63fe3c21fcff28aa6
Gauri Vijay Vispute <104923499+GauriVispute@users.noreply.github.com>
2026-05-08 09:03:36 +0530

    Update print_1_to_n_no_using_while_loop.java
    added a comment for more clear understanding

--- Loops/print_1_to_n_no_using_while_loop.java
+++ Loops/print_1_to_n_no_using_while_loop.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class print_1_to_n_no_using_while_loop {
     public static void main(String [] args){
         Scanner sc  = new Scanner(System.in);
-
+        
         System.out.print("Enter number : ");
+        // n is nothing but the range 
         int n = sc.nextInt();
 
         int counter =1;
