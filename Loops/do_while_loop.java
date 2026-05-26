059c53353723e74f2ff433ad4f5462834b43fa45
Gauri Vijay Vispute <104923499+GauriVispute@users.noreply.github.com>
2026-05-26 20:26:50 +0530

    Create do_while_loop.java
    do while loop

---
+++ Loops/do_while_loop.java
@@ -0,0 +1,9 @@
+public class do_while_loop {
+	public static void do_while_loop(String[] args) {
+		int counter = 1;
+		do{
+			System.out.println("Hello World "+counter);
+			counter++;
+		}while(counter<=10);
+	}
+}
