ca866756721e58b4da4226b6bc41bc8f33f53931
Gauri Vijay Vispute <104923499+GauriVispute@users.noreply.github.com>
2026-05-29 08:32:20 +0530

    Create break_keyword.java
    break keyword

---
+++ Loops/break_keyword.java
@@ -0,0 +1,11 @@
+public class break_keyword {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+			if (i==3){
+				break;
+			}
+			System.out.println(i);
+		}
+			System.out.println("You are out off the loop");
+	}
+}
